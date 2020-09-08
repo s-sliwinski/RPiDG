c59f7485deaa87f0660a0c887ad87dcaa5b3d3a7
patrykpilarski <57600182+patrykpilarski@users.noreply.github.com>
2020-09-08 14:39:36 +0200

    comin' back to project

--- app/src/main/java/com/example/sensehat/COMMON.java
+++ app/src/main/java/com/example/sensehat/COMMON.java
@@ -6,7 +6,7 @@ public final class COMMON {
 
     // configuration info: names and default values
     public final static String CONFIG_IP_ADDRESS = "ipAddress";
-    public final static String DEFAULT_IP_ADDRESS = "192.168.1.18";
+    public final static String DEFAULT_IP_ADDRESS = "192.168.0.24";
 
     public final static String CONFIG_SAMPLE_TIME = "sampleTime";
     public final static int DEFAULT_SAMPLE_TIME = 500;
